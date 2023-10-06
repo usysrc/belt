b6843a89ca8140c5440e7cb69ee77a26e6de8692
usysrc <usysrc@usysrc.dev>
2023-10-06 18:49:35 +0200

    should contain an error

--- install.sh
+++ install.sh
@@ -2,14 +2,15 @@
 
 # List of tool repositories to install
 tools=(
-   github.com/usysrc/uuid
-   github.com/usysrc/ssl-expiry
-   github.com/usysrc/timezone
-   github.com/usysrc/xls-format
-   # Add more tool repositories as needed
+	github.com/usysrc/uuid
+	github.com/usysrc/ssl-expiry
+	github.com/usysrc/timezone
+	github.com/usysrc/xls-format
+	# Add more tool repositories as needed
 )
 
 # Install each tool
+ls | grep 1
 for tool in "${tools[@]}"; do
-   go install "$tool@latest"
+	go install "$tool@latest"
 done
