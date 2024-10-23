451eb781d8f66b4bd62719705879deb2be1bbe76
usysrc <usysrc@usysrc.dev>
2024-10-23 23:31:40 +0200

    docs: correct description of list

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 
 # List of tool repositories to install
 tools=(
-	github.com/usysrc/uuid
-	github.com/usysrc/ssl-expiry
-	github.com/usysrc/timezone
-	github.com/usysrc/xls-format
-	github.com/usysrc/serve
-	github.com/usysrc/jenv
-	# Add more tool repositories as needed
+  github.com/usysrc/uuid
+  github.com/usysrc/ssl-expiry
+  github.com/usysrc/timezone
+  github.com/usysrc/xls-format
+  github.com/usysrc/serve
+  github.com/usysrc/jenv
+  # Add more tool repositories here
 )
 
 # Install each tool
 for tool in "${tools[@]}"; do
-	go install "$tool@latest"
+  go install "$tool@latest"
 done
