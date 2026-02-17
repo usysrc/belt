3fa02244faa821fd1da535e9e0c11f99dcf795fa
usysrc <usysrc@usysrc.dev>
2026-02-17 23:24:20 +0100

    refactor(obs): viper to interface

--- obs/internal/config/config.go
+++ obs/internal/config/config.go
@@ -3,12 +3,14 @@ package config
 import (
 	"fmt"
 	"os"
-
-	"github.com/spf13/viper"
 )
 
-func GetVault(v *viper.Viper) (string, error) {
-	vault := v.GetString("vault")
+type Getter interface {
+	GetString(key string) string
+}
+
+func GetVault(g Getter) (string, error) {
+	vault := g.GetString("vault")
 	if vault == "" {
 		return "", fmt.Errorf("vault not configured. Run 'obs config --vault \"Your Vault\"' first")
 	}
@@ -16,18 +18,18 @@ func GetVault(v *viper.Viper) (string, error) {
 	return vault, nil
 }
 
-func GetTargetFolder(v *viper.Viper) (string, error) {
-	targetFolder := v.GetString("targetFolder")
+func GetTargetFolder(g Getter) (string, error) {
+	targetFolder := g.GetString("targetFolder")
 
 	return targetFolder, nil
 }
 
-func GetVaultPath(v *viper.Viper) (string, error) {
+func GetVaultPath(g Getter) (string, error) {
 	if envVaultPath := os.Getenv("OBS_VAULT_PATH"); envVaultPath != "" {
 		return envVaultPath, nil
 	}
 
-	vaultPath := v.GetString("vaultPath")
+	vaultPath := g.GetString("vaultPath")
 	if vaultPath == "" {
 		return "", fmt.Errorf(
 			"vault path not configured. Run 'obs config --vaultPath \"/path/to/vault\"' or set OBS_VAULT_PATH",
